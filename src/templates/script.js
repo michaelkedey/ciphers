c62949c9682b464d46203c5f3a1d438978159c31
michaelkedey <privatemyk@gmail.com>
2024-08-27 19:58:55 +0000

    cipher

---
+++ src/templates/script.js
@@ -0,0 +1,7 @@
+function copyToClipboard(text) {
+    navigator.clipboard.writeText(text).then(function() {
+        alert('Copied to clipboard!');
+    }, function(err) {
+        console.error('Could not copy text: ', err);
+    });
+}
