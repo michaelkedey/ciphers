d46bdb644047d8911367de24eb9a7b7b3132195f
michaelkedey <privatemyk@gmail.com>
2024-09-04 13:00:25 +0000

    update3

---
+++ src/v2/static/scripts/script.js
@@ -0,0 +1,8 @@
+function copyToClipboard(text) {
+    navigator.clipboard.writeText(text).then(function() {
+        alert('Copied to clipboard!');
+    }, function(err) {
+        console.error('Could not copy text: ', err);
+    });
+}
+
